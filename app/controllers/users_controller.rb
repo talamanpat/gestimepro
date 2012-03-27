6a4ae60a6702fdb66a28bd984a813de8a5ed51ed
Daniel Tala <talaman@hotmail.cl>
2012-03-27 16:59:58 -0400

    validacion modelos 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to users_path, :notice => "Signed up!"
     else
       render "new"
     end
