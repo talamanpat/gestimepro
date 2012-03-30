1f1fa7eb297a6d9917207580bf2fc39d225fa28e
Daniel Tala <talaman@hotmail.cl>
2012-03-30 07:40:45 -0400

    layout login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,22 @@
 class SessionsController < ApplicationController
   before_filter :check_auth, :except => [:new, :create]
   def new
+    render :layout => 'clean'
   end
 
   def create
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to root_url, :notice => "Logueado!"
     else
-      flash.now.alert = "Invalid email or password"
+      flash.now.alert = "Email o password invalido"
       render "new"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to log_in_path, :notice => "Deslogueado!"
   end
 end
